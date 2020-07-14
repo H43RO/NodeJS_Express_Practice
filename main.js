7e96b265ee3db645884d4b844e8f98d6cf9c13aa
H43RO <h2is1234@naver.com>
2020-07-14 14:05:25 +0900

    Implementation POST with Express

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
-
+var qs = require('querystring');
 
 app.get('/', function(request, response) {
     fs.readdir('./data', function(error, filelist) {
@@ -42,6 +42,40 @@ app.get('/page/:pageId', function(request, response) {
         });
     });
 
+});
+
+app.get('/create', function(request, response) {
+    fs.readdir('./data', function(error, filelist) {
+        var title = 'WEB - create';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
+              <form action="/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                  <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+            `, '');
+        response.send(html);
+    });
+});
+
+app.post('/create_process', function(request, response) {
+    var body = '';
+    request.on('data', function(data) {
+        body = body + data;
+    });
+    request.on('end', function() {
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+            response.redirect(302, '/page/${title}')
+        })
+    });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000'));
