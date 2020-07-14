33e3dad270a1be4f0e59c4f4d9ebcc2b8f1ce902
H43RO <h2is1234@naver.com>
2020-07-14 15:40:43 +0900

    Migration code with Body-parser(Node.js MiddleWare)

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const app = express();
 var fs = require('fs');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
+
+const express = require('express');
+const app = express();
+
+//Body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response) {
     fs.readdir('./data', function(error, filelist) {
@@ -41,7 +46,6 @@ app.get('/page/:pageId', function(request, response) {
             response.send(html);
         });
     });
-
 });
 
 app.get('/create', function(request, response) {
@@ -64,18 +68,12 @@ app.get('/create', function(request, response) {
 });
 
 app.post('/create_process', function(request, response) {
-    var body = '';
-    request.on('data', function(data) {
-        body = body + data;
-    });
-    request.on('end', function() {
-        var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-            response.redirect(302, '/page/${title}')
-        })
-    });
+    var post = request.body;
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+        response.redirect(302, '/page/${title}')
+    })
 });
 
 app.get('/update/:pageId', function(request, response) {
@@ -106,36 +104,24 @@ app.get('/update/:pageId', function(request, response) {
 });
 
 app.post('/update_process', function(request, response) {
-    var body = '';
-    request.on('data', function(data) {
-        body = body + data;
-    });
-    request.on('end', function() {
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, function(error) {
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-                response.redirect(302, `/page/${title}`);
-            })
-        });
+    var post = request.body;
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+            response.redirect(302, `/page/${title}`);
+        })
     });
 });
 
 app.post('/delete_process', function(request, response) {
-    var body = '';
-    request.on('data', function(data) {
-        body = body + data;
-    });
-    request.on('end', function() {
-        var post = qs.parse(body);
-        var id = post.id;
-        var filteredId = path.parse(id).base;
-        fs.unlink(`data/${filteredId}`, function(error) {
-            response.redirect(`/`);
-        })
-    });
+    var post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error) {
+        response.redirect(`/`);
+    })
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000'));
