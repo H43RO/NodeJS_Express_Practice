cc4b7097abbfd62aed029a78d422a75bc56045af
H43RO <h2is1234@naver.com>
2020-07-14 14:25:41 +0900

    Implementation UPDATE, DELETE with Express

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ app.get('/page/:pageId', function(request, response) {
             var html = template.HTML(sanitizedTitle, list,
                 `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
                 ` <a href="/create">create</a>
-                    <a href="/update?id=${sanitizedTitle}">update</a>
-                    <form action="delete_process" method="post">
+                    <a href="/update/${sanitizedTitle}">update</a>
+                    <form action="/delete_process" method="post">
                       <input type="hidden" name="id" value="${sanitizedTitle}">
                       <input type="submit" value="delete">
                     </form>`
@@ -74,6 +74,66 @@ app.post('/create_process', function(request, response) {
         var description = post.description;
         fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
             response.redirect(302, '/page/${title}')
+        })
+    });
+});
+
+app.get('/update/:pageId', function(request, response) {
+    fs.readdir('./data', function(error, filelist) {
+        var filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+            var title = request.params.pageId;
+            var list = template.list(filelist);
+            var html = template.HTML(title, list,
+                `
+                <form action="/update_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                  <p>
+                    <textarea name="description" placeholder="description">${description}</textarea>
+                  </p>
+                  <p>
+                    <input type="submit">
+                  </p>
+                </form>
+                `,
+                `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+            );
+            response.writeHead(200);
+            response.end(html);
+        });
+    });
+});
+
+app.post('/update_process', function(request, response) {
+    var body = '';
+    request.on('data', function(data) {
+        body = body + data;
+    });
+    request.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error) {
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+                response.redirect(302, `/page/${title}`);
+            })
+        });
+    });
+});
+
+app.post('/delete_process', function(request, response) {
+    var body = '';
+    request.on('data', function(data) {
+        body = body + data;
+    });
+    request.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(error) {
+            response.redirect(`/`);
         })
     });
 });
